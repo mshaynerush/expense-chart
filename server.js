da53e52336eb4eeec7394730d0c9f0a76e82d69f
mshaynerush <mshaynerush@gmail.com>
2022-08-27 14:06:05 -0700

    Tyring to add copy repo

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expLayout = require('express-ejs-layouts')
-const person = { id: '2345', name: 'Shayne', car: "Escape Hybrid" }
 const indexRouter = require('./routes/index')
 
 app.set('view engine', 'ejs')
@@ -18,14 +17,12 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.DBCONNECT, {
     useNewUrlParser: true
 })
+
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 
-app.get('/shop', (req, res) => {
-    res.render('shop.ejs')
-})
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT)
